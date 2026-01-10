c4113186709d8390e8a7c03d73d2d87ac57848d3
Krishna Sharma <Krishnasharma283125@gmail.com>
2026-01-10 15:02:21 +0530

    Custom Exception

--- Day13/CustomException.cs
+++ Day13/CustomException.cs
@@ -1,6 +1,16 @@
 public class AppCustomException : Exception
 {
-    public override string Message => "Internal Exception";
+    // public override string Message => "Internal Exception"; // one way to do it
+    public override string Message => HandleBase(base.Message);
+
+    private string HandleBase(string sysMessage)
+    {
+        // Original Message from base class
+        Console.WriteLine(sysMessage);
+
+        return "Internal Exception Occurred. Please contact Admin";
+    }
+    
 }
 
 
